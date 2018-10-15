f7e786b339142c37ee52c2996cb9f95a3298aa0c
happyriver <nick.river72@gmail.com>
2018-10-15 16:35:31 -0500

    Lap_input

--- lab_input/input_ip.py
+++ lab_input/input_ip.py
@@ -1,3 +1,6 @@
 #!/usr/bin/env python3.6
+# Lab for input
 user_input = input('Please enter an IPv4 address:')
 print("You told me the IPv4 address is:" + user_input)
+dev_vendor = input('Please enter the vendor name of the swtich:')
+print("You told me the vendor of the switch is:" + dev_vendor)
