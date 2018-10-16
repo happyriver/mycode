533c35f0a1ea9a53a75466f75c11ddaad65e1f93
happyriver <nick.river72@gmail.com>
2018-10-16 12:05:09 -0500

    adding

--- if-test/if-hostname.py
+++ if-test/if-hostname.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3.6
-hostname = 'MTG'
-if hostname == 'MTG':
+hostname = input('Please enter the hostname:')
+if hostname.upper() == 'MTG':
    print('The hostname was found to be ', hostname)
+
+print('Exiting the script')
