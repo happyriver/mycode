b965648a67815b7a4535dcbb918935761b35d1fb
happyriver <nick.river72@gmail.com>
2018-10-16 11:29:26 -0500

    add mix_list3

---
+++ addon.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3.6
+# Function
+def getuanswer():
+  uanswer = input('What is the file you are lookin for?')
+  return(uanswer)
+
---
+++ mix-list3/animal-list.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3.6
+animallist = ['Fox', 'Fly', 'Ant', 'Bee', 'Cat']
+for i in animallist:
+   print (i)
